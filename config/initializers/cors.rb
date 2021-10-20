b870045cf2bbb0ab16006b23519a1acdaae07bd5
Henrique Albuquerque <henrique.alb@me.com>
2021-10-20 16:22:50 +0100

    Created Users table with devise

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins '*' # TODO: change in production
 
     resource '*',
       headers: :any,
