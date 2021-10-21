9a6a13e027c172541d80256ee5a88810e06ede5e
Ines Louro <inespazlouro@gmail.com>
2021-10-21 16:48:35 +0100

    Defined routes for patients. Implemented index and show methods in patients controller.

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,6 +1,9 @@
 class PatientsController < ApplicationController
+  before_action :set_patient, only: %i[show]
+
   def index
     @patients = Patient.all
+    render json: @patients
   end
 
   def create
@@ -13,11 +16,22 @@ class PatientsController < ApplicationController
   end
 
   def show
+    render json: @patient
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def set_patient
+    @patient = Patient.find(params[:id])
+  end
+
+  def patient_params
+    params.require(:patient).permit(:full_name, :name, :dob, :is_active, :citizen_no, :nif_no, :health_no, :social_security_no, :clothes_tag, :sex)
   end
 end
