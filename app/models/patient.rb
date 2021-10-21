42c4d9268f252939a375484658efa977ac449fa9
Henrique Albuquerque <henrique.alb@me.com>
2021-10-21 21:38:32 +0100

    Added uniqueness validation to patient document numbers

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -6,6 +6,7 @@ class Patient < ApplicationRecord
   has_many :patient_exits, through: :patient_admissions
 
   validates :full_name, presence: true
+  validates :citizen_no, :nif_no, :health_no, :social_security_no, uniqueness: true
 
   enum sex: { female: 1, male: 2 }
 end
