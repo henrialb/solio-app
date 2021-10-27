404203a1b2173f685223d5d07f60801a0c05297d
Inês Louro <ines.louro@MBP-de-Ines.lan>
2021-10-27 18:10:22 +0100

    added fields to patient form

--- client/src/components/patient/PatientForm.jsx
+++ client/src/components/patient/PatientForm.jsx
@@ -10,17 +10,31 @@ class PatientForm extends Component {
     this.state = {
       patient: {
         id: this.getPatientId(props),
-        name: '',
         full_name: '',
+        name: '',
         sex: '',
+        dob: '',
+        citizen_no: '',
+        nif_no: '',
+        health_no: '',
+        social_security_no: '',
+        clothes_tag: '',
+        is_active: '', // TODO: only allow admin profile to be able to change this
       },
       redirect: null,
       errors: []
     }
 
-    this.setName = this.setName.bind(this)
     this.setfullName = this.setfullName.bind(this)
-    this.setSex = this.setSex.bind(this)
+    this.setName = this.setName.bind(this)
+    this.setSex = this.setSex.bind(this)    
+    this.setDob = this.setDob.bind(this)
+    this.setCitizenNo = this.setCitizenNo.bind(this)
+    this.setNifNo = this.setNifNo.bind(this)
+    this.setHealthNo = this.setHealthNo.bind(this)
+    this.setSocialSecurityNo = this.setSocialSecurityNo.bind(this)
+    this.setClothesTag = this.setClothesTag.bind(this)
+    this.setIsActive = this.setIsActive.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
   }
 
@@ -31,20 +45,55 @@ class PatientForm extends Component {
       return null
     }
   }
+  
+  setfullName(event) {
+    let newVal = event.target.value || ''
+    this.setFieldState('fullName', newVal)
+  }
 
   setName(event) {
     let newVal = event.target.value || ''
     this.setFieldState('name', newVal)
   }
 
-  setfullName(event) {
-    let newVal = event.target.value || ''
-    this.setFieldState('fullName', newVal)
-  }
-
   setSex(event) {
     let newVal = event.target.value || ''
     this.setFieldState('sex', newVal)
+  }
+
+  setDob(event) {
+    let newVal = event.target.value || ''
+    this.setFieldState('dob', newVal)
+  }
+
+  setCitizenNo(event) {
+    let newVal = event.target.value || ''
+    this.setFieldState('citizenNo', newVal)
+  }
+
+  setNifNo(event) {
+    let newVal = event.target.value || ''
+    this.setFieldState('nifNo', newVal)
+  }
+
+  setHealthNo(event) {
+    let newVal = event.target.value || ''
+    this.setFieldState('healthNo', newVal)
+  }
+
+  setSocialSecurityNo(event) {
+    let newVal = event.target.value || ''
+    this.setFieldState('socialSecurityNo', newVal)
+  }
+
+  setClothesTag(event) {
+    let newVal = event.target.value || ''
+    this.setFieldState('clothesTag', newVal)
+  }
+
+  setIsActive(event) {
+    let newVal = event.target.value || ''
+    this.setFieldState('isActive', newVal)
   }
 
   setFieldState(field, newVal) {
@@ -59,9 +108,16 @@ class PatientForm extends Component {
     event.preventDefault()
 
     let patient = {
-      name: this.state.patient.name,
       full_name: this.state.patient.fullName,
-      sex: this.state.patient.sex
+      name: this.state.patient.name,
+      sex: this.state.patient.sex,
+      dob: this.state.patient.dob,
+      citizenNo: this.state.patient.citizenNo,
+      nifNo: this.state.patient.nifNo,
+      healthNo: this.state.patient.healthNo,
+      socialSecurityNo: this.state.patient.socialSecurityNo,
+      clothesTag: this.state.patient.clothesTag,
+      isActive: this.state.patient.sex
     }
 
     Api.savePatient(patient, this.state.patient.id)
@@ -125,16 +181,40 @@ class PatientForm extends Component {
 
               <form onSubmit={this.handleSubmit}>
                 <div className="input-group">
-                  <label htmlFor="name" className="form-label">Name</label>
-                  <input type="text" name="name" id="name" value={patient.name} placeholder="Enter name" onChange={this.setName} className="form-control" />
-                </div>
-                <div className="input-group">
                   <label htmlFor="fullName" className="form-label">Full name</label>
                   <input type="text" name="fullName" id="fullName" value={patient.fullName} placeholder="Enter full name" onChange={this.setfullName} className="form-control" />
                 </div>
                 <div className="input-group">
+                  <label htmlFor="name" className="form-label">Name</label>
+                  <input type="text" name="name" id="name" value={patient.name} placeholder="Enter name" onChange={this.setName} className="form-control" />
+                </div>
+                <div className="input-group">
                   <label htmlFor="sex" className="form-label">Sex</label>
                   <input type="text" name="sex" id="sex" value={patient.sex} placeholder="Enter sex" onChange={this.setSex} className="form-control" />
+                </div>
+                <div className="input-group">
+                  <label htmlFor="dob" className="form-label">Date of birth</label>
+                  <input type="date" name="dob" id="dob" value={patient.dob} placeholder="Enter date of birth" onChange={this.setDob} className="form-control" />
+                </div>
+                <div className="input-group">
+                  <label htmlFor="citizenNo" className="form-label">Citizen number</label>
+                  <input type="text" name="citizenNo" id="citizenNo" value={patient.citizenNo} placeholder="Enter citizen number" onChange={this.setCitizenNo} className="form-control" />
+                </div>
+                <div className="input-group">
+                  <label htmlFor="nifNo" className="form-label">Nif number</label>
+                  <input type="text" name="nifNo" id="nifNo" value={patient.nifNo} placeholder="Enter nif number" onChange={this.setNifNo} className="form-control" />
+                </div>
+                <div className="input-group">
+                  <label htmlFor="healthNo" className="form-label">Health number</label>
+                  <input type="text" name="healthNo" id="healthNo" value={patient.healthNo} placeholder="Enter health number" onChange={this.setHealthNo} className="form-control" />
+                </div>
+                <div className="input-group">
+                  <label htmlFor="socialSecurityNo" className="form-label">Social security number</label>
+                  <input type="text" name="socialSecurityNo" id="socialSecurityNo" value={patient.socialSecurityNo} placeholder="Enter social security number" onChange={this.setSocialSecurityNo} className="form-control" />
+                </div>
+                <div className="input-group">
+                  <label htmlFor="isActive" className="form-label">Is active?</label>
+                  <input type="text" name="isActive" id="isActive" value={patient.isActive} placeholder="Is active?" onChange={this.setIsActive} className="form-control" />
                 </div>
                 <button className="btn btn-success">Submit</button>
               </form>
