f4347aa367c9239e179cc2962b52633cc7ea214e
Henrique Albuquerque <henrique.alb@me.com>
2021-10-28 18:10:59 +0100

    patient details works

--- client/src/components/patient/PatientDetails.jsx
+++ client/src/components/patient/PatientDetails.jsx
@@ -1,59 +1,60 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Redirect } from 'react-router'
 
 const Api = require('./Api.js')
 
-class PatientDetails extends Component {
-  constructor(props) {
-    super(props)
+const PatientDetails = ({match}) => {
 
-    this.state = {
-      id: props.match.params.id,
-      redirect: null
-    }
-  }
+  const [patient, setPatient] = useState({});
+  const [redirect, setRedirect] = useState(null) // TODO: setRedirect is never used
+  const [errors, setErrors] = useState(null)
+  const id = match?.params?.id
 
-  componentDidMount() {
-    if (this.state.patient.id) {
-      Api.getPatient(this.state.patient.id)
+  useEffect(() => {
+    if(id) {
+      Api.getPatient(id)
         .then(response => {
           const [error, data] = response
-          if (error) {
-            this.setState({
-              errors: data
-            })
-          } else {
-            this.setState({
-              patient: data,
-              errors: []
-            })
+          if(error) {
+            return setErrors(data)
           }
+          setPatient(data)
+
+          // if (error) {
+          //   this.setState({
+          //     errors: data
+          //   })
+          // } else {
+          //   this.setState({
+          //     patient: data,
+          //     errors: []
+          //   })
+          // }
         })
     }
-  }
+  }, [id]) // with an empty array the code will only run once
 
-  render() {
-    const { redirect, patient } = this.state
-
-    if (redirect) {
-      return (
-        <Redirect to={redirect} />
-      )
-    } else {
-      return (
-        <>
-          <p>{patient.fullName}</p>
-          <p>{patient.name}</p>
-          <p>{patient.sex}</p>
-          <p>{patient.dob}</p>
-          <p>{patient.citizenNo}</p>
-          <p>{patient.nifNo}</p>
-          <p>{patient.healthNo}</p>
-          <p>{patient.socialSecurityNo}</p>
-          <p>{patient.clothesTag}</p>
-        </>
-      )
+  if (redirect) {
+    return (
+      <Redirect to={redirect} />
+    )
+  } else {
+    if(errors) {
+      return <h1>ups</h1>
     }
+    return (
+      <>
+        <p>{patient.fullName}</p>
+        <p>{patient.name}</p>
+        <p>{patient.sex}</p>
+        <p>{patient.dob}</p>
+        <p>{patient.citizenNo}</p>
+        <p>{patient.nifNo}</p>
+        <p>{patient.healthNo}</p>
+        <p>{patient.socialSecurityNo}</p>
+        <p>{patient.clothesTag}</p>
+      </>
+    )
   }
 }
 
