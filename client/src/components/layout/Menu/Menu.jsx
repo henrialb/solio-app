acad9df30fd7c96c739b377f3e2fd4b7e2a8c721
Henrique Albuquerque <henrique.alb@me.com>
2021-10-30 15:39:13 +0100

    refactored code in App.jsx

--- client/src/components/layout/Menu/Menu.jsx
+++ client/src/components/layout/Menu/Menu.jsx
@@ -11,32 +11,6 @@ const Menu = () => {
       <MenuItem icon="calendar-alt" label="Visitas" path="/visitas" />
       <MenuItem icon="mortar-pestle" label="Farmácia" path="/farmacia" />
       <MenuItem icon="cubes" label="Stocks" path="/stocks" />
-
-
-      {/* <div className="menu-item py-3">
-        <i className="fas fa-home d-block fs-3 pb-1"></i>
-        <span className="fw-light">Início</span>
-      </div>
-      <div className="menu-item py-3">
-        <i className="fas fa-user-friends d-block fs-3 pb-1"></i>
-        <span className="fw-light">Utentes</span>
-      </div>
-      <div className="menu-item py-3">
-        <i className="fas fa-receipt d-block fs-3 pb-1"></i>
-        <span className="fw-light">Despesas</span>
-      </div>
-      <div className="menu-item py-3">
-        <i className="fas fa-calendar-alt d-block fs-3 pb-1"></i>
-        <span className="fw-light">Visitas</span>
-      </div>
-      <div className="menu-item py-3">
-        <i className="fas fa-mortar-pestle d-block fs-3 pb-1"></i>
-        <span className="fw-light">Farmácia</span>
-      </div>
-      <div className="menu-item py-3">
-        <i className="fas fa-cubes d-block fs-3 pb-1"></i>
-        <span className="fw-light">Stocks</span>
-      </div> */}
     </div>
   )
 }
