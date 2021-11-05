a671f375a7bbf9ac9cfbd01acbdd5285062e5c71
Henrique Albuquerque <henrique.alb@me.com>
2021-11-05 21:21:44 +0000

    added transformers to Blueprinter (camelCase and remove nil fields) – WIP

--- app/blueprints/patient_blueprint.rb
+++ app/blueprints/patient_blueprint.rb
@@ -1,6 +1,9 @@
 class PatientBlueprint < Blueprinter::Base
   identifier :id
 
+  # TODO: make LowerCamelTransformer work (in app/lib)
+
+  # fields :name, :dob, :sex, :full_name, :full_name, :is_active, :citizen_no, :nif_no, :health_no, :social_security_no, :clothes_tag
   fields :name, :dob, :sex
 
   field :full_name, name: :fullName
