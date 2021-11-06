9bcbb6699cbc25734192db7d230dd79dcb731023
Henrique Albuquerque <henrique.alb@me.com>
2021-11-06 03:06:07 +0000

    Fixed app/lib file names, which were preventing classes from being loaded

--- app/blueprints/patient_blueprint.rb
+++ app/blueprints/patient_blueprint.rb
@@ -1,16 +1,5 @@
 class PatientBlueprint < Blueprinter::Base
   identifier :id
 
-  # TODO: make LowerCamelTransformer work (in app/lib)
-
-  # fields :name, :dob, :sex, :full_name, :full_name, :is_active, :citizen_no, :nif_no, :health_no, :social_security_no, :clothes_tag
-  fields :name, :dob, :sex
-
-  field :full_name, name: :fullName
-  field :is_active, name: :isActive
-  field :citizen_no, name: :citizenNo
-  field :nif_no, name: :nifNo
-  field :health_no, name: :healthNo
-  field :social_security_no, name: :socialSecurityNo
-  field :clothes_tag, name: :clothesTag
+  fields :name, :dob, :sex, :full_name, :full_name, :is_active, :citizen_no, :nif_no, :health_no, :social_security_no, :clothes_tag
 end
