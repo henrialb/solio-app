74c7e94159d9b7f149e4ccf4d85fb98df744bf87
Henrique Albuquerque <henrique.alb@me.com>
2021-11-18 12:14:29 +0000

    Added patients#active

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
+
+  get 'patients/active', to: 'patients#active'
+
   resources :patients, only: %i[index show create destroy update]
   resources :patient_admissions, only: %i[index show create destroy update]
   resources :patient_files, only: %i[index show create destroy update]
