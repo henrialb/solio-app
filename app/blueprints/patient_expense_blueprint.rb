baae8f8906e818b243ee3ccb1a4a386e3430cf9e
Ines Louro <inespazlouro@gmail.com>
2022-02-12 17:47:43 +0000

    added fields to patient expenses blueprint

--- app/blueprints/patient_expense_blueprint.rb
+++ app/blueprints/patient_expense_blueprint.rb
@@ -1,5 +1,5 @@
 class PatientExpenseBlueprint < Blueprinter::Base
   identifier :id
 
-  fields :description, :amount, :note, :date, :patient_file_id
+  fields :description, :amount, :note, :date, :patient_file_id, :patient_id, :patient_receivable_id
 end
