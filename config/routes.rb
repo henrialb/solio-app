bfb6a7725f9f44758e073ee221290e48aca6515f
Henrique Albuquerque <henrique.alb@me.com>
2022-02-22 12:12:41 +0000

    Add route to create_from_expenses

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,16 @@ Rails.application.routes.draw do
   devise_for :users
 
   get 'patients/all', to: 'patients#all'
-
   resources :patients, only: %i[index show create destroy update]
+
+  resources :patient_receivables, except: %i[add edit]
+  post 'patient_receivables/create_from_expenses', to: 'patient_receivables#create_from_expenses'
+
   resources :patient_admissions, only: %i[index show create destroy update]
   resources :patient_files, only: %i[index show create destroy update]
   resources :patient_expenses, only: %i[index show create destroy update]
   resources :patient_relatives, only: %i[index show create destroy update]
   resources :patient_exits, only: %i[index show create destroy update]
-  resources :patient_receivables, only: %i[index show create destroy update]
   resources :employees, only: %i[index show create destroy update]
   resources :employee_admissions, only: %i[index show create destroy update]
   resources :employee_exits, only: %i[index show create destroy update]
