846cb0b1d04cf38acc4761899be38b5798b61544
Henrique Albuquerque <henrique.alb@me.com>
2022-02-22 01:46:46 +0000

    fix some validations and associations

--- app/models/patient_admission.rb
+++ app/models/patient_admission.rb
@@ -1,7 +1,7 @@
 class PatientAdmission < ApplicationRecord
   belongs_to :patient
   has_many :patient_files, dependent: :destroy
-  has_many :patient_exits, dependent: :destroy
+  has_one :patient_exit, dependent: :destroy
 
   validates :date, presence: true
 end
