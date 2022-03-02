ab945083efb669fc48754c04d98a5ed4c8bcf4cd
Henrique Albuquerque <henrique.alb@me.com>
2022-03-02 18:23:14 +0000

    Handle payment on create

--- app/controllers/patient_payments_controller.rb
+++ app/controllers/patient_payments_controller.rb
@@ -9,6 +9,21 @@ class PatientPaymentsController < ApplicationController
   def create
     @patient_payment = PatientPayment.new(patient_payment_params)
     if @patient_payment.save
+      patient = Patient.find(@patient_payment.patient_id)
+      unpaid_receivables = PatientReceivable.where(patient_id: patient).unpaid.order(amount: :asc)
+      funds = @patient_payment.amount + patient.balance
+
+      unpaid_receivables.each do |receivable|
+        if receivable.amount <= funds
+          receivable.update(status: "paid", patient_payment_id: @patient_payment.id)
+          funds -= receivable.amount
+        else
+          break
+        end
+      end
+
+      patient.update(balance: funds) if funds != patient.balance
+
       render json: PatientPaymentBlueprint.render(@patient_payment)
     else
       render json: @patient_payment.errors, status: :unprocessable_entity
