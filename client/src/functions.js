6a1233d82fe0cd85e67520c324606d05e1a4e0c1
Henrique Albuquerque <henrique.alb@me.com>
2022-03-11 16:27:45 +0000

    Separate client to a new repo

--- client/src/functions.js
+++
@@ -1,9 +0,0 @@
-const age = (dob) => {
-  var dobdate = new Date(dob)
-  var diff_ms = Date.now() - dobdate.getTime()
-  var age_dt = new Date(diff_ms)
-
-  return Math.abs(age_dt.getUTCFullYear() - 1970)
-}
-
-export { age }
