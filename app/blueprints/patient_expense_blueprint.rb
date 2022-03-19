14d344562ca5d812fe3d0b6d88854d6fef3691cf
Henrique Albuquerque <henrique.alb@me.com>
2022-03-19 17:07:21 +0000

    Add route to get expenses from a specific patient

--- app/blueprints/patient_expense_blueprint.rb
+++ app/blueprints/patient_expense_blueprint.rb
@@ -2,4 +2,8 @@ class PatientExpenseBlueprint < Blueprinter::Base
   identifier :id
 
   fields :description, :amount, :note, :date, :patient_file_id, :patient_id, :patient_receivable_id
+
+  field :receivable_status do |patient_expense|
+    patient_expense.patient_receivable.status unless patient_expense.patient_receivable.nil?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   get 'patients/all', to: 'patients#all'
   resources :patients, except: %i[add edit]
 
+  get 'patient_expenses/patient/:id', to: 'patient_expenses#patient'
   resources :patient_expenses, except: %i[add edit]
+
   resources :patient_receivables, except: %i[add edit]
   post 'patient_receivables/create_from_expenses', to: 'patient_receivables#create_from_expenses'
   post 'patient_receivables/create_from_monthly_fee', to: 'patient_receivables#create_from_monthly_fee'
