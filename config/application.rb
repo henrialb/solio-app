92ff2720381a01ba1f71de513fd47080c554c19d
Ines Louro <inespazlouro@gmail.com>
2022-03-31 18:00:25 +0100

    small fix

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,5 @@ module SolioApp
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.action_controller.session_store
   end
 end
