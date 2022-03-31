8ac6d12ee8020a739c27cdeccd7a378c5b13c292
Ines Louro <inespazlouro@gmail.com>
2022-03-31 15:48:45 +0100

    added fake data seeds as comment

--- db/seeds.rb
+++ db/seeds.rb
@@ -80,6 +80,92 @@ end
 puts 'Done creating patient admissions, files and exits'
 puts '---------------'
 
+puts 'Done seeding the database 💪'
+
+
+# # ------------------ FAKE DATA SEEDS -----------------
+
+# puts 'About to spread seeds 🌱'
+# puts '---------------'
+
+# # Patients
+# puts 'Destroying patients and associated records'
+# Patient.destroy_all
+
+# names = %w(Maria Ana Joana Isabel Catarina Rita Vitória Luísa Raquel Aurora)
+# surnames = %w(Ribeiro Pereira Domingues Cruz Silva Fernandes Antunes Machado Fontes Pinheiro)
+# mensalidades = [1590, 1640, 1670, 1790]
+
+# puts 'Creating patients'
+
+# 11.times do
+#   all_names = [names.sample, names.sample, surnames.sample, surnames.sample]
+
+#   Patient.create!(
+#     name: all_names.first,
+#     full_name: all_names.join(' '),
+#     dob: Faker::Date.birthday(min_age: 73, max_age: 104),
+#     covenant: [:personal, :scml].sample,
+#     sex: [:female, :male].sample,
+#     status: [:active, :inactive].sample,
+#     citizen_no: Faker::Number.number(digits: 8),
+#     nif_no: Faker::Number.number(digits: 9),
+#     health_no: Faker::Number.number(digits: 9),
+#     social_security_no: Faker::Number.number(digits: 11),
+#     clothes_tag: "#{all_names.first.first}#{all_names.second.first}#{all_names.last.first}",
+#     monthly_fee: mensalidades.sample,
+#     balance: Faker::Commerce.price(range: 0..10.0)
+#   )
+# end
+
+# puts 'Done creating patients'
+# puts '---------------'
+
+# # Patient Admissions, Files and Exits
+# puts 'Creating patient admissions'
+# patients = Patient.all
+
+# patients.each do |patient|
+#   PatientAdmission.create!(patient_id: patient.id, date: Faker::Date.between(from: '1997-11-17', to: Date.today))
+# end
+
+# puts 'Creating patient files and exits'
+# admissions = PatientAdmission.all
+
+# admissions.each do |admission|
+#   facility = [:'36', :'21'].sample
+
+#   if admission.id.odd?
+#     file = PatientFile.create!(
+#       patient_admission_id: admission.id,
+#       facility: facility,
+#       open_date: admission.date,
+#       close_date: Faker::Date.between(from: admission.date, to: Date.today),
+#       note: [nil, Faker::Lorem.sentence].sample
+#     )
+
+#     PatientExit.create!(
+#       patient_admission_id: admission.id,
+#       date: file.close_date,
+#       reason: ['saiu', 'faleceu'].sample,
+#       location: Faker::TvShows::Simpsons.location,
+#       note: [nil, Faker::Lorem.sentence].sample
+#     )
+#   else
+#     PatientFile.create!(patient_admission_id: admission.id, facility: facility, open_date: admission.date)
+#   end
+# end
+
+# # Create a second admission for first patient
+# puts 'Adding a new admission and file for first patient'
+
+# admission = PatientAdmission.create!(patient_id: Patient.first.id, date: Faker::Date.between(from: PatientExit.first.date, to: Date.today))
+
+# PatientFile.create!(patient_admission_id: admission.id, open_date: admission.date, facility: [:'36', :'21'].sample)
+
+# puts 'Done creating patient admissions, files and exits'
+# puts '---------------'
+
 # # Patient Expenses, Receivables and Payments
 # puts 'Creating patient expenses, receivables and payments'
 
@@ -173,4 +259,4 @@ puts '---------------'
 # puts 'Done creating patient expenses, receivables and payments'
 # puts '---------------'
 
-puts 'Done seeding the database 💪'
+# puts 'Done seeding the database 💪'
