0c4c6ed96bc94c72cf4f0c799191e1156c78597a
Ines Louro <inespazlouro@gmail.com>
2022-04-27 22:44:23 +0100

    fixed credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,5 @@ class User < ApplicationRecord
 
   enum role: { admin: 0, manager: 1, nurse: 2, carer: 3, family: 4 }
 
-  # has_one :employee, dependent: :destroy
-  # has_many :visits, dependent: :destroy
-
   validates :role, presence: true
 end
