953d5661f886a68072964fdda10ec284d9351b89
Henrique Albuquerque <henrique.alb@me.com>
2022-05-10 16:50:38 +0100

    Expose Authorization token in response headers

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
 
-  self.skip_session_storage = [:http_auth, :params_auth]
-
   enum role: { admin: 0, manager: 1, nurse: 2, carer: 3, family: 4 }
 
   validates :role, presence: true
